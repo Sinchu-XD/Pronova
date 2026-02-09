d09fde66979832d104fc3578531d259b23802039
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-09 05:30:01 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,13 +1,11 @@
 from pyrogram import Client
 from AbhiCalls import VoiceEngine
 
-# CONFIG
 API_ID = 35362137
 API_HASH = "API_HASH"
 BOT_TOKEN = "BOT_TOKEN"
 SESSION_STRING = "SESSION"
 
-# Clients
 bot = Client(
     "music_bot",
     api_id=API_ID,
@@ -22,10 +20,4 @@ user = Client(
     session_string=SESSION_STRING
 )
 
-# Voice Engine
 engine = VoiceEngine(user)
-
-# Assistant info
-ASSISTANT_ID = None
-ASSISTANT_USERNAME = None
-
