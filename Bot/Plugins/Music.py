d7f56cfd1d5f20da818f7b3c98bb72fbd7b9edb1
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-09 05:33:02 +0530

    Add play and playforce commands for music playback

---
+++ Bot/Plugins/Music.py
@@ -0,0 +1,66 @@
+from pyrogram import filters
+from Bot import bot, engine
+from Bot.Helper.Assistant import get_ass
+from Bot.Helper.Font import sc
+
+
+@bot.on_message(filters.command("play"))
+async def play(_, m):
+    try:
+        await m.delete()
+    except:
+        pass
+
+    if not await get_ass(m.chat.id, m):
+        return
+
+    reply = m.reply_to_message
+
+    if reply and (reply.voice or reply.audio):
+        path = await reply.download()
+        song, _ = await engine.vc.play_file(
+            m.chat.id, path, m.from_user.mention, reply=reply
+        )
+        if not song:
+            await m.reply(sc("unable to play audio"))
+        return
+
+    if len(m.command) < 2:
+        return await m.reply(sc("usage play song name"))
+
+    query = m.text.split(None, 1)[1]
+    song, _ = await engine.vc.play(m.chat.id, query, m.from_user.mention)
+
+    if not song:
+        await m.reply(sc("unable to play song"))
+
+
+@bot.on_message(filters.command("playforce"))
+async def playforce(_, m):
+    try:
+        await m.delete()
+    except:
+        pass
+
+    if not await get_ass(m.chat.id, m):
+        return
+
+    await engine.vc.stop(m.chat.id)
+
+    reply = m.reply_to_message
+
+    if reply and (reply.voice or reply.audio):
+        path = await reply.download()
+        song, _ = await engine.vc.play_file(
+            m.chat.id, path, m.from_user.mention, reply=reply
+        )
+        if not song:
+            await m.reply(sc("force play failed"))
+        return
+
+    if len(m.command) < 2:
+        return await m.reply(sc("usage playforce song"))
+
+    query = m.text.split(None, 1)[1]
+    await engine.vc.play(m.chat.id, query, m.from_user.mention)
+    
