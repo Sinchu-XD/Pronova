df22282adbf131a0652419c3f46d9263d6d5bbdb
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-11 17:10:38 +0530

    Update CallBacks.py

--- Bot/Plugins/CallBacks.py
+++ Bot/Plugins/CallBacks.py
@@ -1,38 +1,70 @@
 from pyrogram.enums import ChatMemberStatus
+
 from Bot import bot, engine
 from Bot.Helper.Font import sc
+
+# ===== DB =====
+from Bot.Database.Bans import is_banned, is_gbanned
 
 
 @bot.on_callback_query()
 async def vc_buttons(_, cq):
-    m = cq.message
-    chat_id = m.chat.id
+    try:
+        m = cq.message
+        chat_id = m.chat.id
+        user = cq.from_user
 
-    member = await bot.get_chat_member(chat_id, cq.from_user.id)
-    if member.status not in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER):
-        return await cq.answer(sc("only admins"), show_alert=True)
+        if not user:
+            return await cq.answer()
 
-    mention = cq.from_user.mention
+        uid = user.id
 
-    if cq.data == "vc_skip":
-        await engine.vc.skip(chat_id)
-        await m.reply(sc("song skipped by") + " " + mention)
+        # ===== BAN CHECK =====
+        if await is_gbanned(uid):
+            return await cq.answer(sc("you are gbanned"), show_alert=True)
 
-    elif cq.data == "vc_end":
-        await engine.vc.stop(chat_id)
-        await m.reply(sc("playback ended by") + " " + mention)
+        if await is_banned(uid):
+            return await cq.answer(sc("you are banned"), show_alert=True)
 
-    elif cq.data == "vc_pause":
-        await engine.vc.pause(chat_id)
-        await m.reply(sc("paused by") + " " + mention)
+        # ===== ADMIN CHECK =====
+        member = await bot.get_chat_member(chat_id, uid)
 
-    elif cq.data == "vc_resume":
-        await engine.vc.resume(chat_id)
-        await m.reply(sc("resumed by") + " " + mention)
+        if member.status not in (
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER
+        ):
+            return await cq.answer(sc("only admins"), show_alert=True)
 
-    elif cq.data == "vc_previous":
-        ok = await engine.vc.previous(chat_id)
-        if not ok:
-            await m.reply(sc("no previous song"))
+        mention = user.mention
 
-    await cq.answer()
+        # ===== ACTIONS =====
+        if cq.data == "vc_skip":
+            await engine.vc.skip(chat_id)
+            await m.reply(sc("song skipped by") + " " + mention)
+
+        elif cq.data == "vc_end":
+            await engine.vc.stop(chat_id)
+            await m.reply(sc("playback ended by") + " " + mention)
+
+        elif cq.data == "vc_pause":
+            await engine.vc.pause(chat_id)
+            await m.reply(sc("paused by") + " " + mention)
+
+        elif cq.data == "vc_resume":
+            await engine.vc.resume(chat_id)
+            await m.reply(sc("resumed by") + " " + mention)
+
+        elif cq.data == "vc_previous":
+            ok = await engine.vc.previous(chat_id)
+            if not ok:
+                await m.reply(sc("no previous song"))
+
+        await cq.answer()
+
+    except Exception as e:
+        print("Callback Error:", e)
+        try:
+            await cq.answer("Error", show_alert=True)
+        except:
+            pass
+            
