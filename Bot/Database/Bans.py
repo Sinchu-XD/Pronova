5a89e9b8073d56bdb76271a4a1798620860e14e1
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-11 18:19:33 +0530

    Update Bans.py

--- Bot/Database/Bans.py
+++ Bot/Database/Bans.py
@@ -3,27 +3,37 @@ from .Core import db
 from .Stats import inc_lifetime
 
 
-async def ban_user(user_id):
+# ================= BAN (PER CHAT) =================
+async def ban_user(chat_id, user_id):
     try:
-        await db.banned.insert_one({"user_id": user_id})
+        await db.banned.insert_one({
+            "chat_id": chat_id,
+            "user_id": user_id
+        })
         await inc_lifetime("banned")
     except DuplicateKeyError:
         pass
 
 
-async def unban_user(user_id):
-    await db.banned.delete_one({"user_id": user_id})
+async def unban_user(chat_id, user_id):
+    await db.banned.delete_one({
+        "chat_id": chat_id,
+        "user_id": user_id
+    })
 
 
-async def is_banned(user_id):
-    return await db.banned.find_one({"user_id": user_id})
+async def is_banned(chat_id, user_id):
+    return await db.banned.find_one({
+        "chat_id": chat_id,
+        "user_id": user_id
+    })
 
 
 async def total_banned():
     return await db.banned.count_documents({})
 
 
-# gban
+# ================= GBAN (GLOBAL) =================
 async def gban_user(user_id):
     try:
         await db.gbanned.insert_one({"user_id": user_id})
@@ -38,4 +48,3 @@ async def ungban_user(user_id):
 
 async def is_gbanned(user_id):
     return await db.gbanned.find_one({"user_id": user_id})
-  
