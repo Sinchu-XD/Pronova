9ba6340bf784506ea5988786779a536995214b28
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-11 18:31:28 +0530

    Update Core.py

--- Bot/Database/Core.py
+++ Bot/Database/Core.py
@@ -12,8 +12,16 @@ async def setup_database():
     await db.chats.create_index("chat_id", unique=True)
     await db.group_stats.create_index("chat_id", unique=True)
     await db.songs_stats.create_index("title", unique=True)
-    await db.banned.create_index("user_id", unique=True)
+
+    # ===== FIXED BAN INDEX =====
+    await db.banned.create_index(
+        [("chat_id", 1), ("user_id", 1)],
+        unique=True
+    )
+
+    # global ban correct
     await db.gbanned.create_index("user_id", unique=True)
+
     await db.daily.create_index("date", unique=True)
     await db.gc_activity.create_index("chat_id", unique=True)
     await db.afk.create_index("user_id", unique=True)
