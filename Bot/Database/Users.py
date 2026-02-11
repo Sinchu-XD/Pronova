14729a0ae1dfc1c1c758fddecc1c319d5c3f4ec1
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-11 15:51:44 +0530

    Create Users.py

---
+++ Bot/Database/Users.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from pymongo.errors import DuplicateKeyError
+from .core import db
+from .stats import inc_lifetime, inc_daily
+
+
+async def add_user(user):
+    if not user or user.is_bot:
+        return
+
+    try:
+        await db.users.insert_one({
+            "user_id": user.id,
+            "join_date": datetime.utcnow(),
+        })
+        await inc_lifetime("users")
+        await inc_daily("users")
+    except DuplicateKeyError:
+        pass
+
+
+async def total_users():
+    return await db.users.count_documents({})
+
+
+async def get_users():
+    async for u in db.users.find({}):
+        yield u["user_id"]
+      
