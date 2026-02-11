02e86ae116a3e8ba23a6329f98a9ad0f79dbac75
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-12 03:16:20 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -120,7 +120,7 @@ async def main():
 
     # ===== START AUTOMATION =====
     print("📊 starting daily report scheduler")
-    asyncio.create_task(safe_task(daily_gc_report(bot)))
+    asyncio.create_task(safe_task(daily_gc_report(bot), "GetActivicy"))
 
     print("💤 bot running")
     await idle()
