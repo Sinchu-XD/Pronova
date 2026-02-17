9319c4f934df29c08efae73ea1b109884842d651
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-17 17:37:26 +0530

    Update Font.py

--- Bot/Helper/Font.py
+++ Bot/Helper/Font.py
@@ -1,3 +1,9 @@
+import random
+from pyrogram.types import MessageEntity
+from pyrogram import enums
+from Bot import CUSTOM_EMOJI_IDS
+
+
 SMALL_CAPS = {
     "a": "ᴀ", "b": "ʙ", "c": "ᴄ", "d": "ᴅ", "e": "ᴇ",
     "f": "ғ", "g": "ɢ", "h": "ʜ", "i": "ɪ", "j": "ᴊ",
@@ -8,7 +14,7 @@ SMALL_CAPS = {
 }
 
 
-def sc(text: str) -> str:
+def sc(text: str, entities: bool = False):
     if not text:
         return ""
 
@@ -30,4 +36,30 @@ def sc(text: str) -> str:
 
         result.append(first + "".join(rest_chars))
 
-    return f"**{' '.join(result)}**"
+    styled = f"**{' '.join(result)}**"
+
+    # ===== PREMIUM PART =====
+    left_id = random.choice(CUSTOM_EMOJI_IDS)
+    right_id = random.choice(CUSTOM_EMOJI_IDS)
+
+    final_text = f"❤️ {styled} ❤️"
+
+    premium_entities = [
+        MessageEntity(
+            type=enums.MessageEntityType.CUSTOM_EMOJI,
+            offset=0,
+            length=1,
+            custom_emoji_id=left_id
+        ),
+        MessageEntity(
+            type=enums.MessageEntityType.CUSTOM_EMOJI,
+            offset=len(final_text) - 1,
+            length=1,
+            custom_emoji_id=right_id
+        )
+    ]
+
+    if entities:
+        return final_text, premium_entities
+
+    return final_text
